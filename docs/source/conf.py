482d8570192a59cee44e406a6f10a225c544fec2
SanCriolloB <santicriollo@hotmail.com>
2025-11-21 10:23:57 -0500

    docs: configurar Sphinx con MyST y acceso al backend

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,40 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+# --- Paths para encontrar el backend (paquete neurocampus) ---
+ROOT_DIR = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
+BACKEND_SRC = os.path.join(ROOT_DIR, "backend", "src")
+sys.path.insert(0, BACKEND_SRC)
+
 project = 'NeuroCampus'
 copyright = '2025, Daniel Felipe Ramirez Cabrera & Santiago Criollo Bermudez'
 author = 'Daniel Felipe Ramirez Cabrera & Santiago Criollo Bermudez'
 release = '0.1.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
 language = 'es'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# --- Extensiones de Sphinx ---
+extensions = [
+    "myst_parser",           # Markdown con MyST
+    "sphinx.ext.autodoc",    # API Python
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",   # docstrings estilo Google/NumPy
+    "sphinx.ext.viewcode",
+]
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+autosummary_generate = True
+
+# Configuración MyST (para poder usar toctree en .md)
+myst_enable_extensions = [
+    "deflist",
+    "colon_fence",
+]
+
+# Tema
+html_theme = "sphinx_rtd_theme"
+
+templates_path = ["_templates"]
+exclude_patterns = []
+html_static_path = ["_static"]
